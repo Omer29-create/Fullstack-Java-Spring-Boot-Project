de22f80ea321702aa00286193ceb377ddbf172b1
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-05 16:27:35 +0200

    Update UserRepository.java

--- src/main/java/com/deneme/BosturDeneyebilirsin/Repository/UserRepository.java
+++ src/main/java/com/deneme/BosturDeneyebilirsin/Repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.deneme.BosturDeneyebilirsin.Repository;
 
+import com.deneme.BosturDeneyebilirsin.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User getUserById(Long id);
+
+    User updateUserById(User user);
+  
+    User deleteById(Long id);
 }
